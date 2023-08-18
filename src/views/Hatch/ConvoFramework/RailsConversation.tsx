5f56a957a3a1258819ddb1ea563ad46a59c20be0
Chris Moore <chrisdmoore06@gmail.com>
2023-08-18 07:32:12 -0400

    asdasd

--- src/views/Hatch/ConvoFramework/RailsConversation.tsx
+++ src/views/Hatch/ConvoFramework/RailsConversation.tsx
@@ -83,7 +83,7 @@ const RailsConversation: React.FC = () => {
 
   return (
     <>
-      <main className="h-[calc(100vh-80px)] flex flex-col-reverse">
+      <main className="h-[calc(100vh-100px)] flex flex-col-reverse">
         <MessageList messages={messages} messagesEndRef={messagesEndRef} />
 
         <FooterContentCard>
